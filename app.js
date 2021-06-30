f735bda883d5b6916a26beb6d44c09175a518182
EmadBeshtawi <>
2021-06-30 13:27:52 +0000

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+var name = prompt("what's you'r name?")
+console.log(name)
+var age = prompt("what's you'r Age?")
+console.log(age)
+alert("welcome to Top-Series " + name + ":)" )
+var favSeries = prompt("what's you'r fav Series?")
+console.log(favSeries)
+
+var parts = prompt("How many parts do you want to watch?")
+console.log(parts)
+
+
+if (parts < 11) {
+  alert("Enjoy");
+} else {
+  alert("Enjoy");
+}
+
+
+
+
