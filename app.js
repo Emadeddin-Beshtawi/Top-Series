ba81d4d67c21674862c3d55511147e8029b350c4
EmadBeshtawi <>
2021-07-02 14:01:16 +0000

    up

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-var favSeries = prompt("What is your favSeries? Game of Thrones OR Vikings");
+alert('WELCOME')
 
-if( favSeries == "Vikings" ){
-var parts= prompt("How many parts do you want to watch? ")
+var favSeries = prompt("What is your favSeries? Game of Thrones or Vikings");
 
-for(var i = 0 ; i < parts; i++) {
-document.write("<div>" + "<h3>" + favSeries +"</h3>" + "<img src='https://m.media-amazon.com/images/M/MV5BODk4ZjU0NDUtYjdlOS00OTljLTgwZTUtYjkyZjk1NzExZGIzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg' alt='Vikings' width='200' height='200'>" + "</div>")
-}
-while(parts > 10){
-  parts = prompt("Please enter less than 10 parts to see your favSeries")
+while (favSeries != "Game of Thrones" && favSeries != "Vikings") {
+  favSeries = prompt("Kindly input either Game of Thrones or Vikings")
 }
 
-}
-else if ( favSeries == "Game of Thrones"){
-  var parts= prompt("How many parts do you want to watch? ")
+var parts = prompt(' How many parts of ' + favSeries + ' do you want to watch? ' );
+for (var i=0; i< parts ; i++) {
+  if (favSeries == 'Game of Thrones'){
+    document.write('<img src="https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg" alt="Game of Thrones"  width="200" height="200">')
+  }else if (favSeries == 'Vikings'){
+     document.write('<img src="https://m.media-amazon.com/images/M/MV5BODk4ZjU0NDUtYjdlOS00OTljLTgwZTUtYjkyZjk1NzExZGIzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg" alt="Vikings" width="200" height="200">')
 
-  for(var i = 0 ; i < parts; i++) {
-document.write("<div>" + "<h3>" + favSeries +"</h3>" + "<img src='https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg' alt='Game of Thrones' width='200' height='200'>" + "</div>")
+  }
+
 }
 
-while(parts > 10){
-  parts = prompt("Please enter less than 10 parts to see your favSeries")
-}}
+// if( favSeries == "Vikings" ){
+// var parts= prompt("How many parts do you want to watch? ")
+
+// for(var i = 0 ; i < parts; i++) {
+// document.write("<div>" + "<h3>" + favSeries +"</h3>" + "<img src='https://m.media-amazon.com/images/M/MV5BODk4ZjU0NDUtYjdlOS00OTljLTgwZTUtYjkyZjk1NzExZGIzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg' alt='Vikings' width='200' height='200'>" + "</div>")
+// }
+// while(parts > 10){
+//   parts = prompt("Please enter less than 10 parts to see your favSeries")
+// }
+
+// }
+// else if ( favSeries == "Game of Thrones"){
+//   var parts= prompt("How many parts do you want to watch? ")
+
+//   for(var i = 0 ; i < parts; i++) {
+// document.write("<div>" + "<h3>" + favSeries +"</h3>" + "<img src='https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg' alt='Game of Thrones' width='200' height='200'>" + "</div>")
+// }
+
+// while(parts > 10){
+//   parts = prompt("Please enter less than 10 parts to see your favSeries")
+// }}
